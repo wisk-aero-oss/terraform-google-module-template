8bac6c12723a3c0e5f4fecd46370d672de855490
Steven Nemetz <steven.nemetz@wisk.aero>
2025-09-08 10:29:16 -0700

    Update org references

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 /**
  * # terraform-google-module-template
  *
- * [![Releases](https://img.shields.io/github/v/release/notablehealth/terraform-google-module-template)](https://github.com/notablehealth/terraform-google-module-template/releases)
+ * [![Releases](https://img.shields.io/github/v/release/wisk-aero-oss/terraform-google-module-template)](https://github.com/wisk-aero-oss/terraform-google-module-template/releases)
  *
- * [Terraform Module Registry](https://registry.terraform.io/modules/notablehealth/module-template/google)
+ * [Terraform Module Registry](https://registry.terraform.io/modules/wisk-aero-oss/module-template/google)
  *
  * Template for creating a Terraform module for Google
  *
